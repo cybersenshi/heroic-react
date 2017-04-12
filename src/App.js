669eba6e170d57a06350a3df3cb9b6748729c8aa
Chaney <lh-chan@mechapilot.org>
2017-04-12 00:30:22 -0400

    Break all the things! Then fix them. Srsly, still just playing at this point.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <header>
+            <div className="navbar-fixed">
+                <nav className="grey darken-4">
+                    <div className="nav-wrapper">
+                        <ul className="right">
+                            <li>
+                                <a href="/">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/creations">
+                                    Creations
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/tools">
+                                    Tools
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/about">
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/contact">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+          <h1>
+              <a href="/">
+                  Heroic Webcraft
+              </a>
+          </h1>
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
